19f6a6f0a7c1184ce3b934de059ada708cf3d2df
rutvik jarwal <154890655+rutwikjarwal@users.noreply.github.com>
2024-12-17 12:15:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// define A FUNCTION FOR CAL TIP AMOUNT 
+
+
+function lipCalculator()
+{
+
+    let ba,tp,np,pamt,tpamtx,fbamt;  // local variable 
+    // data coll
+     ba =+bamt.value ;
+     np =+nop.value;
+     tp =+tip.value;
+
+//    cals  s 
+
+     pamt =ba*tp/100;
+     tpamt=pamt*np;
+     fbamt=ba+tpamt;
+       // displaying op`s
+       tippp.value="tip par parson &nbsp "+pamt;
+       tottip.value="total tip"+tpamt;
+       totbill.innerHTML="total amount "+fbamt;
+
+
+}
